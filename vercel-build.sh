1e7216fc0afe02a986519fe22b2dc7848f4adaa9
omar-el01 <omaargily@gmail.com>
2025-10-29 22:41:54 +0100

    Configure Django for production on Vercel

--- vercel-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-echo ">>> Running database migrations..."
-python3 triplog_backend/manage.py migrate
-
-echo ">>> Migrations complete."
