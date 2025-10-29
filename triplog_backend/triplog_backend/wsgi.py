5bd00eff0e4f3e742d5fe2f4bf4bbd60eefe6ee3
omar-el01 <omaargily@gmail.com>
2025-10-29 22:04:05 +0100

    Configure Django for production on Vercel

--- triplog_backend/triplog_backend/wsgi.py
+++ triplog_backend/triplog_backend/wsgi.py
@@ -1,6 +1,20 @@
 
 import os
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
+
+CURRENT_DIR = Path(__file__).resolve().parent
+
+
+PROJECT_ROOT = CURRENT_DIR.parent
+
+sys.path.insert(0, str(PROJECT_ROOT))
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'triplog_backend.settings')
-app = get_wsgi_application()
+
+application = get_wsgi_application()
+
+app = application
