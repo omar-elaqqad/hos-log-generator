4ef85f6df59217ae0f317e37870ae7344acc9dcb
omar-el01 <omaargily@gmail.com>
2025-10-29 16:42:33 +0100

    Configure for Vercel deployment

--- triplog_backend/triplog_backend/wsgi.py
+++ triplog_backend/triplog_backend/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trip_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'triplog_backend.trip_backend.settings')
 app = get_wsgi_application()
